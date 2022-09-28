d6ed9b44c9956b7a30a0bc7b4f5878d99a89a757
Zobachov Vladimir <zobachov@gmail.com>
2022-09-29 02:01:03 +0500

    fix: 2nd edit password min value

--- backend/validator.js
+++ backend/validator.js
@@ -12,14 +12,14 @@ const validateLink = (link) => {
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(validateLink),
